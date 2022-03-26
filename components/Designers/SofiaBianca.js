89cdb18f7157fbf8cb682d7cf1d1003136ee4def
unknown <trulygorgeous@outlook.com>
2022-03-26 20:15:39 +0000

    first commit2

--- components/Designers/SofiaBianca.js
+++ components/Designers/SofiaBianca.js
@@ -8,54 +8,65 @@ import SliderContainerPhone from "../SliderContainerPhone/SliderContainerPhone";
 import {
   DesignersContainer,
   GridPhotoContainer,
-
   ModelNameTitle,
   NameDesigner,
+  NameDesignerSub,
   ProductContainer,
   ProductContainerPhone,
 } from "./DesignersStyles";
 
-import MainPhoto from './MainPhoto'
+import MainPhoto from "./MainPhoto";
 
 function SofiaBianca() {
   const ronaldModelsObject = designers[6].models;
 
   return (
     <DesignersContainer>
-      <NameDesigner>Sofia Bianca</NameDesigner>
+      <NameDesigner style={{ margin: "30px" }}>Sofia Bianca</NameDesigner>
 
       {ronaldModelsObject.map((value, index) => (
-        <ProductContainer key={index}>
-          <MainPhoto front={value.imgs[0]} back={value.imgs[1]} key={index} src={value.imgs[0]}></MainPhoto>
-          <GridPhotoContainer>
-            <ModelNameTitle></ModelNameTitle>
+        <>
+          <ProductContainer key={index}>
+            <MainPhoto
+              front={value.imgs[0]}
+              back={value.imgs[1]}
+              key={index}
+              src={value.imgs[0]}
+            ></MainPhoto>
+            <GridPhotoContainer>
+              <ModelNameTitle></ModelNameTitle>
 
-             <SliderContainer photoMain={value.imgs} i={index} by={0} s/> 
+              <SliderContainer photoMain={value.imgs} i={index} by={0} s />
+            </GridPhotoContainer>
+          </ProductContainer>
 
-          
-          </GridPhotoContainer>
-        </ProductContainer>
+          {value.uid == "3" ? (
+            ""
+          ) : (
+            <NameDesignerSub style={{ margin: "30px" }}>
+              Sofia Bianca
+            </NameDesignerSub>
+          )}
+        </>
       ))}
 
-     {
-       ronaldModelsObject.map((v, i) => (
-          
-          
+      {ronaldModelsObject.map((v, i) => (
+        <>
+          <ProductContainerPhone>
             <>
-            <ProductContainerPhone>
-     
-              
-              <>
-               <SliderContainerPhone a={v}/>
-              </>
-         
-            </ProductContainerPhone>
+              <SliderContainerPhone a={v} />
             </>
-          
 
-       ))
-
-     }
+            {v.uid == "3" ? (
+              ""
+            ) : (
+              <NameDesigner style={{ margin: "30px" }}>
+                Ronald Joyce
+              </NameDesigner>
+            )}
+          </ProductContainerPhone>
+        </>
+      ))}
     </DesignersContainer>
   );
 }

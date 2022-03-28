0b64b93754835de20180e0e5c141eea9b0879590
unknown <trulygorgeous@outlook.com>
2022-03-28 13:57:32 +0100

    first commit2

--- components/Designers/DesignersMain.js
+++ components/Designers/DesignersMain.js
@@ -12,32 +12,45 @@ import {
 
 function DesignersMain() {
   return (
-    <DesignersContainer  style={{
-      "background-image": "url('./assets/backgroundA.jpg')" , 
-      //"backgroundRepeat":"no-repeat",
-      //"backgroundPosition":"center",
-      //"backgroundSize":"cover"
-      }}>
+    <DesignersContainer
+      style={{
+        "background-image": "url('./assets/backgroundA.jpg')",
+        //"backgroundRepeat":"no-repeat",
+        //"backgroundPosition":"center",
+        //"backgroundSize":"cover"
+      }}
+    >
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Acme&display=swap');
       </style>
       <NameDesigner
-      style={{
-        'color':'rgb(115 112 137)'
-      }}
-      
-      >Designers</NameDesigner>
-      <DesignersMainContainer style={{'marginTop':'50px'}}>
+        style={{
+          color: "rgb(115 112 137)",
+        }}
+      >
+        Designers
+      </NameDesigner>
+      <DesignersMainContainer style={{ marginTop: "50px" }}>
         <DesignersCardContainer>
-          <LinkA href={"/ronald-joyce"}>
+          <LinkA href={"/paloma-blanca"}>
             <DesignersMainCard>
-            <TitleBrand>
-                <p className="brandTitle">Ronald Joyce</p>
+              <TitleBrand>
+                <p className="brandTitle">Paloma Blanca</p>
               </TitleBrand>
             </DesignersMainCard>
           </LinkA>
 
+          <LinkA href={"/ronald-joyce"}>
+            <DesignersMainCard>
+              <TitleBrand>
+                <p className="brandTitle">Ronald Joyce</p>
+              </TitleBrand>
+            </DesignersMainCard>
+          </LinkA>
+        </DesignersCardContainer>
+
+        <DesignersCardContainer>
           <LinkA href={"/victoria-jane"}>
             <DesignersMainCard>
               <TitleBrand>
@@ -45,17 +58,37 @@ function DesignersMain() {
               </TitleBrand>
             </DesignersMainCard>
           </LinkA>
+          <DesignersMainCard>
+            <a href={"https://dianelegrandbridal.com/"} target="_blank">
+              <TitleBrand>
+                <p className="brandTitle">Diane Legrand</p>
+              </TitleBrand>
+            </a>
+          </DesignersMainCard>
         </DesignersCardContainer>
 
         <DesignersCardContainer>
-          <LinkA href={"/paloma-blanca"}>
+          <LinkA href={"/alan-hannah"}>
             <DesignersMainCard>
-            <TitleBrand>
-                <p className="brandTitle">Paloma Blanca</p>
+              <TitleBrand>
+                <p className="brandTitle">Alan Hannah</p>
               </TitleBrand>
             </DesignersMainCard>
           </LinkA>
 
+          <LinkA href={"/lusan-mandongus"}>
+            <DesignersMainCard>
+              <TitleBrand>
+                <p className="brandTitle">Lusan Mandongus</p>
+              </TitleBrand>
+            </DesignersMainCard>
+          </LinkA>
+        </DesignersCardContainer>
+
+        <DesignersCardContainer
+          style={{ justifyContent: "left" }}
+          bottom="true"
+        >
           <LinkA href={"/sofia-bianca"}>
             <DesignersMainCard>
               <TitleBrand>
@@ -63,36 +96,6 @@ function DesignersMain() {
               </TitleBrand>
             </DesignersMainCard>
           </LinkA>
-        </DesignersCardContainer>
-
-        <DesignersCardContainer>
-          <LinkA href={"/alan-hannah"}>
-            <DesignersMainCard>
-            <TitleBrand>
-                <p className="brandTitle">Alan Hannah</p>
-              </TitleBrand>
-            </DesignersMainCard>
-          </LinkA>
-
-            <DesignersMainCard>
-          <a href={"https://dianelegrandbridal.com/"} target="_blank">
-              <TitleBrand>
-                <p className="brandTitle">Diane Legrand</p>
-              </TitleBrand>
-          </a>
-            </DesignersMainCard>
-        </DesignersCardContainer>
-
-        <DesignersCardContainer style={{'justifyContent':'left', }} bottom='true'>
-          <LinkA href={"/lusan-mandongus"}>
-            <DesignersMainCard>
-            <TitleBrand>
-                <p className="brandTitle">Lusan Mandongus</p>
-              </TitleBrand>
-            </DesignersMainCard>
-          </LinkA>
-
-        
         </DesignersCardContainer>
       </DesignersMainContainer>
     </DesignersContainer>

c70e7dd3fd74beae75aa3e6fd00dd8411b9942ec
unknown <trulygorgeous@outlook.com>
2022-03-28 13:01:37 +0100

    first commit2

--- components/ContactContainer/ContactContainer.js
+++ components/ContactContainer/ContactContainer.js
@@ -215,7 +215,7 @@ function ContactContainer() {
           <ContactBoxes2>
             <IframeBoxContainer>
               <IframeBox
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10081.628828875313!2d-0.7505814!3d50.8236209!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcf93b380bcdb6e6e!2sTruly%20Gorgeous%20Wedding%20Dresses!5e0!3m2!1ses-419!2suk!4v1645437678011!5m2!1ses-419!2suk"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10081.628828875313!2d-0.7505814!3d50.8236209!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xcf93b380bcdb6e6e!2sTruly%20Gorgeous%20Wedding%20Dresses!5e0!3m2!1ses-419!2suk!4v1645437678011!5m2!1sen-419!2suk"
                 allowFullScreen=""
                 loading="lazy"
               ></IframeBox>
